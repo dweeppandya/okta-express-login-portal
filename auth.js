939be730f2d9ab204a94b483ec0c3340b2bf5df4
Dweep Pandya <dweep.pandya@gmail.com>
2019-04-14 10:35:53 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -13,7 +13,7 @@ const oidc = new ExpressOIDC({
   issuer: process.env.OKTA_ORG_URL + "/oauth2/default",
   client_id: process.env.OKTA_CLIENT_ID,
   client_secret: process.env.OKTA_CLIENT_SECRET,
-  redirect_uri: process.env.OKTA_CALLBACK_URI || "http://localhost:3000/users/callback",
+  redirect_uri: process.env.OKTA_CALLBACK_URI || "http://ec2-54-91-113-11.compute-1.amazonaws.com:3000/users/callback",
   scope: "openid profile",
   routes: {
     login: {
